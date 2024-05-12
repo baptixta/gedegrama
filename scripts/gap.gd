8d4631aa93fd9023e5c4b4101490d5024d4f7c1c
Davi Baptista <daviarcosbaptista@gmail.com>
2024-05-12 17:26:51 -0300

    feat: drag and drop quase funcional

--- scripts/gap.gd
+++ scripts/gap.gd
@@ -1,14 +1,17 @@
 extends StaticBody2D
 
+@export var correct:String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	modulate = Color("#fff", 0.7)
+	modulate = Color("#fff", 0.8)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Global.is_dragging:
-		visible = true
+		modulate = Color("#fff", 1)
 	else:
-		visible = false
+		modulate = Color("#fff", 0.8)
+		
+	
--- scripts/letra.gd
+++ scripts/letra.gd
@@ -4,26 +4,55 @@ extends Control
 var draggable = false
 var is_inside_dropable = false
 var body_ref
+var offset: Vector2
+var initialPos: Vector2
 
+var no_shadow = preload("res://sprites/cube.png")
+var shadow_effect = preload("res://sprites/cube_shadow.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#label recebe a letra escrita no editor
-	$State.text = letter
-
-
-func _on_drop_point_detector_mouse_entered():
-	#if not global.is_dragging:
 	pass
 
 
+func _process(delta):
+	if draggable:
+		if Input.is_action_just_pressed("click"):
+			initialPos = global_position
+			offset = get_global_mouse_position() - global_position
+			Global.is_dragging = true
+		if Input.is_action_pressed("click"):
+			global_position = get_global_mouse_position() - offset
+		elif Input.is_action_just_released("click"):
+			Global.is_dragging = false
+			var tween = get_tree().create_tween()
+			if is_inside_dropable:
+				tween.tween_property(self,"position",body_ref.position,0.2).set_ease(Tween.EASE_OUT)
+			else:
+				tween.tween_property(self,"global_position",initialPos,0.2).set_ease(Tween.EASE_OUT)
+
+func _on_drop_point_detector_mouse_entered():
+	if not Global.is_dragging:
+		draggable = true
+		$Sprite.texture = shadow_effect
+		scale = Vector2(1.05, 1.05)
+
+
 func _on_drop_point_detector_mouse_exited():
-	pass # Replace with function body.
+	if not Global.is_dragging:
+		draggable = false
+		$Sprite.texture = no_shadow
+		scale = Vector2(1, 1)
 
 
 func _on_drop_point_detector_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group('dropable'):
+		is_inside_dropable = true
+		body.modulate = Color("#fff", 1)
+		body_ref = body
 
 
 func _on_drop_point_detector_body_exited(body):
-	pass # Replace with function body.
+	if body.is_in_group('dropable'):
+		is_inside_dropable = false
+		body.modulate = Color("#fff", 0.7)
